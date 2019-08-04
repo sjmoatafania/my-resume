93b277404caa297c8c0655a61cbe27c918e2e731
figagroup <figa@group>
2019-08-04 09:58:25 +0430

    Updates

---
+++ precache-manifest.ace319592daeaacce7428c15105db601.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "519ed38316c8c3854258c6b71eeea59d",
+    "url": "/my-resume/index.html"
+  },
+  {
+    "revision": "6c8ac55edf1e0a55d7ad",
+    "url": "/my-resume/static/css/main.a23519e9.chunk.css"
+  },
+  {
+    "revision": "46129a9da8d5f9c33bd4",
+    "url": "/my-resume/static/js/2.2847c512.chunk.js"
+  },
+  {
+    "revision": "6c8ac55edf1e0a55d7ad",
+    "url": "/my-resume/static/js/main.f856e853.chunk.js"
+  },
+  {
+    "revision": "2f7b259ab1933d650523",
+    "url": "/my-resume/static/js/runtime~main.1423441b.js"
+  }
+]);
